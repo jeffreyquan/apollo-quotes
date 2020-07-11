f4834ec11af2b19bc7b3321dd902ecd9f1d42a9b
Jeffrey Quan <jeffrey.quan@hotmail.com>
2020-07-12 00:10:20 +1000

    ✏️  added Query, Feed, Mutation types and updated existing types

--- server/schema.ts
+++ server/schema.ts
@@ -1,16 +1,45 @@
 const { gql } = require("apollo-server");
 
 export const typeDefs = gql`
+  type Query {
+    feed: Feed!
+  }
+
+  type Feed {
+    quotes: [Quote]!
+  }
+
+  type Mutation {
+    post(content: String!, author: String!, image: String): Quote!
+    signup(
+      name: String!
+      username: String!
+      password: String!
+      confirmationPassword: String!
+      email: String!
+    ): AuthPayload
+    like(quoteId: ID!): Quote!
+  }
+
+  type Subscription {
+    newQuote: Quote
+    newLike: Like
+  }
+
   type Quote {
     id: ID!
+    user: User!
     content: String!
     author: String!
     likes: [Like!]!
+    image: String
   }
 
   type User {
     id: ID!
     username: String!
+    name: String!
+    password: String!
     email: String!
     quotes: [Quote]!
     likes: [Quote]!
@@ -20,5 +49,10 @@ export const typeDefs = gql`
     id: ID!
     quote: Quote!
     user: User!
+  }
+
+  type AuthPayload {
+    token: String
+    user: User
   }
 `;
