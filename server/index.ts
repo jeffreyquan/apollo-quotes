dc1e70488b4140a7673322b47466d6c811bef44a
Jeffrey Quan <jeffrey.quan@hotmail.com>
2020-07-12 12:47:18 +1000

    🔥 set up connection to mongo db and to express server

--- server/index.ts
+++ server/index.ts
@@ -3,29 +3,43 @@ import mongoose from "mongoose";
 import { ApolloServer } from "apollo-server-express";
 import { merge } from "lodash";
 import { typeDefs } from "./schema";
+import { resolvers as queryResolvers } from "./resolvers/Query";
+import { resolvers as mutationResolvers } from "./resolvers/Mutation";
 
 mongoose.Promise = global.Promise;
 
 const db = "mongodb://127.0.0.1/apollo-quotes";
 
-mongoose.set("useFindAndModify", false);
-mongoose
-  .connect(db, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-  })
-  .then(() => console.log("DB connected."))
-  .catch((err) => console.log(err));
+const connectDatabase = () => {
+  try {
+    mongoose.set("useFindAndModify", false);
+    mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    console.log("DB connected.");
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-const server = new ApolloServer({ typeDefs });
+const resolvers = {};
 
 const app = express();
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers: merge(resolvers, queryResolvers, mutationResolvers),
+});
 
 server.applyMiddleware({ app });
 
 const port = 5000;
 
-app.listen(port, () =>
-  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
-);
+app.listen({ port }, async () => {
+  await connectDatabase();
+  console.log(
+    `🚀 Server ready at http://localhost:${port}${server.graphqlPath}`
+  );
+});
