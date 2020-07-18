16e04e7010b7d007dcd325e78f4603063a058d03
Jeffrey Quan <jeffrey.quan@hotmail.com>
2020-07-18 11:00:00 +1000

    🎨 moved feed query and post mutation into Quote resolver file

--- server/resolvers/Quote.ts
+++ server/resolvers/Quote.ts
@@ -1,3 +1,22 @@
 export const resolvers = {
-  Quote: {},
+  Query: {
+    feed: async (parent, args, { Quote }) => {
+      const quotes = await Quote.find({}).exec();
+      return {
+        quotes,
+      };
+    },
+  },
+  Mutation: {
+    post: async (parent, { content, author, image }, { Quote }) => {
+      const newQuote = new Quote({
+        content,
+        author,
+        image,
+      });
+
+      await newQuote.save();
+      return newQuote;
+    },
+  },
 };
