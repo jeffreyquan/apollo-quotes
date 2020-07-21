8e6b603d9d4a61362d80fb72b5c19b22449ecc34
Jeffrey Quan <jeffrey.quan@hotmail.com>
2020-07-22 00:04:49 +1000

    created quote datasource and createPost method

---
+++ server/datasources/quote.ts
@@ -0,0 +1,54 @@
+import { DataSource } from "apollo-datasource";
+import Quote from "../models/Quote";
+import User from "../models/User";
+import Tag from "../models/Tag";
+import { TagInterface } from "../models/Interfaces";
+
+class QuoteAPI extends DataSource {
+  private context;
+
+  initialize(config) {
+    this.context = config.context;
+  }
+
+  async findTags(tags: TagInterface[]) {
+    return Promise.all(tags.map((tag) => Tag.findOne({ name: tag.name })));
+  }
+
+  async createPost({ content, author, image, tags }) {
+    const { user } = this.context;
+    if (!user) return null;
+
+    const fetchedUser = await User.findById(user._id).exec();
+
+    const newQuote = await new Quote({
+      content,
+      author,
+      image,
+    });
+
+    this.findTags(tags).then((data) => {
+      for (let i = 0; i < data.length; i++) {
+        if (!data[i]) {
+          const newTag = new Tag({
+            name: tags[i],
+          });
+          newTag.save();
+          newQuote.tags.push(newTag);
+        } else {
+          newQuote.tags.push(data[i]);
+        }
+      }
+    });
+
+    newQuote.user = user._id;
+    fetchedUser.quotes.push(newQuote);
+
+    fetchedUser.save();
+    newQuote.save();
+
+    return newQuote;
+  }
+}
+
+export default QuoteAPI;
