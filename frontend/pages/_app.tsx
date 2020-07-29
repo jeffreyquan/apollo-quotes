f98e559646cb4a288d331073270b457e632fdf74
Jeffrey Quan <jeffrey.quan@hotmail.com>
2020-07-30 01:30:29 +1000

    wrap Page around each component

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,12 +1,15 @@
 import { ApolloProvider } from "@apollo/client";
 import { useApollo } from "../lib/withApollo";
+import Page from "../components/Page";
 
 export default function App({ Component, pageProps }) {
   const client = useApollo(pageProps.initialApolloState);
 
   return (
     <ApolloProvider client={client}>
-      <Component {...pageProps} />
+      <Page>
+        <Component {...pageProps} />
+      </Page>
     </ApolloProvider>
   );
 }
