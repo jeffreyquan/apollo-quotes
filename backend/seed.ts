327f20869434f7845605b0300bf198413f2b2b42
Jeffrey Quan <jeffrey.quan@hotmail.com>
2020-07-31 01:22:05 +1000

    created seed file

---
+++ backend/seed.ts
@@ -0,0 +1,65 @@
+export const quotes = [
+  {
+    author: "Forrest Gump",
+    content:
+      "Life was like a box of chocolates. You never know what you're gonna get.",
+    image:
+      "https://s30886.pcdn.co/wp-content/uploads/2020/05/gump-18-e1589202904695-1209x1536.jpeg",
+    tags: ["life", "movie"],
+  },
+  {
+    author: "Arthur Fleck (The Joker)",
+    content:
+      "The worst part of having a mental illness is people expect you to behave as if you don't.",
+    image:
+      "https://m.media-amazon.com/images/M/MV5BZDdiNmEzNzMtMDMxOS00Njg3LThhMGUtYTViZmI3MGNlYjQzXkEyXkFqcGdeQXVyNDIyNjA2MTk@._V1_.jpg",
+    tags: ["mental illness", "movie"],
+  },
+  {
+    author: "Wayne Gretzky",
+    content: "You miss 100% of the shots you don't take.",
+    image:
+      "https://cdn.britannica.com/99/193199-050-B96E48A0/Wayne-Gretzky-2007.jpg",
+    tags: ["inspiration", "sport"],
+  },
+  {
+    author: "David Goggins",
+    content:
+      "You are in danger of living a life so comfortable and soft, that you will die without ever realizing your true potential.",
+    image:
+      "https://www.ahigherbranch.com/wp-content/uploads/2019/06/David-Goggins.jpg",
+    tags: ["inspiration", "mental toughness"],
+  },
+  {
+    author: "Steve Jobs",
+    content:
+      "Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma -- which is living with the results of other people's thinking.",
+    image:
+      "https://cdn.britannica.com/04/171104-050-AEFE3141/Steve-Jobs-iPhone-2010.jpg",
+    tags: ["life", "motivation"],
+  },
+  {
+    author: "Sun Tzu",
+    content:
+      "The art of war teaches us to rely not on the likelihood of the enemy's not coming, but on our own readiness to receive him; not on the chance of his not attacking, but rather on the fact that we have made our position unassailable.",
+    image:
+      "https://vignette.wikia.nocookie.net/deadliestfiction/images/1/1a/Image-2.jpeg/revision/latest/top-crop/width/360/height/450?cb=20180121014622",
+    tags: ["war", "preparedness"],
+  },
+  {
+    author: "Mark Twain",
+    content:
+      "Twenty years from now you will be more disappointed by the things that you didn’t do than by the ones you did do.",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRF8nTcTl4pzLVV6Kf0kjgIi_7ci-ffg9RYtQ&usqp=CAU",
+    tags: ["regret", "inspiration"],
+  },
+  {
+    author: "Bruce Lee",
+    content:
+      "You must be shapeless, formless, like water. When you pour water in a cup, it becomes the cup. When you pour water in a bottle, it becomes the bottle. When you pour water in a teapot, it becomes the teapot. Water can drip and it can crash. Become like water my friend.",
+    image:
+      "https://i.pinimg.com/originals/d5/19/08/d51908d8a586337145d3d271042cb287.jpg",
+    tags: ["present", "adaptable", "legend"],
+  },
+];
