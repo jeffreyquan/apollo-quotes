db2bd29560648bd2ab98db0e6b5a003afa76a00b
Jeffrey Quan <jeffrey.quan@hotmail.com>
2020-08-02 22:56:23 +1000

    🌱 updated seed items to include updated image urls and large image urls

--- server/seed/items.ts
+++ server/seed/items.ts
@@ -4,7 +4,9 @@ export const items = [
     content:
       "Life was like a box of chocolates. You never know what you're gonna get.",
     image:
-      "https://s30886.pcdn.co/wp-content/uploads/2020/05/gump-18-e1589202904695-1209x1536.jpeg",
+      "https://res.cloudinary.com/jq/image/upload/v1596371860/apollo-quotes/nptaosswcqnv2nkoeezd.jpg",
+    largeImage:
+      "https://res.cloudinary.com/jq/image/upload/c_scale,h_600,q_auto/v1596371860/apollo-quotes/nptaosswcqnv2nkoeezd.jpg",
     tags: ["life", "movie"],
   },
   {
@@ -12,14 +14,18 @@ export const items = [
     content:
       "The worst part of having a mental illness is people expect you to behave as if you don't.",
     image:
-      "https://m.media-amazon.com/images/M/MV5BZDdiNmEzNzMtMDMxOS00Njg3LThhMGUtYTViZmI3MGNlYjQzXkEyXkFqcGdeQXVyNDIyNjA2MTk@._V1_.jpg",
+      "https://res.cloudinary.com/jq/image/upload/v1596371856/apollo-quotes/ixq2zyvlnskcbdsuol1c.jpg",
+    largeImage:
+      "https://res.cloudinary.com/jq/image/upload/c_scale,h_600,q_auto/v1596371856/apollo-quotes/ixq2zyvlnskcbdsuol1c.jpg",
     tags: ["mental illness", "movie"],
   },
   {
     author: "Wayne Gretzky",
     content: "You miss 100% of the shots you don't take.",
     image:
-      "https://cdn.britannica.com/99/193199-050-B96E48A0/Wayne-Gretzky-2007.jpg",
+      "https://res.cloudinary.com/jq/image/upload/v1596371866/apollo-quotes/zhv6mle6dh8ridhnmmnw.jpg",
+    largeImage:
+      "https://res.cloudinary.com/jq/image/upload/c_scale,h_600,q_auto/v1596371866/apollo-quotes/zhv6mle6dh8ridhnmmnw.jpg",
     tags: ["inspiration", "sport"],
   },
   {
@@ -27,7 +33,9 @@ export const items = [
     content:
       "You are in danger of living a life so comfortable and soft, that you will die without ever realizing your true potential.",
     image:
-      "https://www.ahigherbranch.com/wp-content/uploads/2019/06/David-Goggins.jpg",
+      "https://res.cloudinary.com/jq/image/upload/v1596371857/apollo-quotes/rih38x07k2kpkz95e2k8.jpg",
+    largeImage:
+      "https://res.cloudinary.com/jq/image/upload/c_scale,h_600,q_auto/v1596371857/apollo-quotes/rih38x07k2kpkz95e2k8.jpg",
     tags: ["inspiration", "mental toughness"],
   },
   {
@@ -35,7 +43,9 @@ export const items = [
     content:
       "Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma -- which is living with the results of other people's thinking.",
     image:
-      "https://cdn.britannica.com/04/171104-050-AEFE3141/Steve-Jobs-iPhone-2010.jpg",
+      "https://res.cloudinary.com/jq/image/upload/v1596371901/apollo-quotes/ko2sxnmdvsmbcxexa8or.jpg",
+    largeImage:
+      "https://res.cloudinary.com/jq/image/upload/c_scale,h_600,q_auto/v1596371901/apollo-quotes/ko2sxnmdvsmbcxexa8or.jpg",
     tags: ["life", "motivation"],
   },
   {
@@ -43,7 +53,9 @@ export const items = [
     content:
       "The art of war teaches us to rely not on the likelihood of the enemy's not coming, but on our own readiness to receive him; not on the chance of his not attacking, but rather on the fact that we have made our position unassailable.",
     image:
-      "https://vignette.wikia.nocookie.net/deadliestfiction/images/1/1a/Image-2.jpeg/revision/latest/top-crop/width/360/height/450?cb=20180121014622",
+      "https://res.cloudinary.com/jq/image/upload/v1596371883/apollo-quotes/nztplykkunrlm0iumkus.jpg",
+    largeImage:
+      "https://res.cloudinary.com/jq/image/upload/c_scale,h_600,q_auto/v1596371883/apollo-quotes/nztplykkunrlm0iumkus.jpg",
     tags: ["war", "preparedness"],
   },
   {
@@ -51,7 +63,9 @@ export const items = [
     content:
       "Twenty years from now you will be more disappointed by the things that you didn’t do than by the ones you did do.",
     image:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRF8nTcTl4pzLVV6Kf0kjgIi_7ci-ffg9RYtQ&usqp=CAU",
+      "https://res.cloudinary.com/jq/image/upload/v1596371896/apollo-quotes/nqmffvvhhbcz8rrmhkvb.jpg",
+    largeImage:
+      "https://res.cloudinary.com/jq/image/upload/c_scale,h_600,q_auto/v1596371896/apollo-quotes/nqmffvvhhbcz8rrmhkvb.jpg",
     tags: ["regret", "inspiration"],
   },
   {
@@ -59,7 +73,9 @@ export const items = [
     content:
       "You must be shapeless, formless, like water. When you pour water in a cup, it becomes the cup. When you pour water in a bottle, it becomes the bottle. When you pour water in a teapot, it becomes the teapot. Water can drip and it can crash. Become like water my friend.",
     image:
-      "https://i.pinimg.com/originals/d5/19/08/d51908d8a586337145d3d271042cb287.jpg",
+      "https://res.cloudinary.com/jq/image/upload/v1596371856/apollo-quotes/gdu3u9d42gbd1sbytfdc.jpg",
+    largeImage:
+      "https://res.cloudinary.com/jq/image/upload/c_scale,h_600,q_auto/v1596371856/apollo-quotes/gdu3u9d42gbd1sbytfdc.jpg",
     tags: ["present", "adaptable", "legend"],
   },
 ];
