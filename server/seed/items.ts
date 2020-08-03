5fdb7eee799309643e47d9bbc60a70f90c3bd5d7
Jeffrey Quan <jeffrey.quan@hotmail.com>
2020-08-03 17:26:08 +1000

    🌱 updated seed items to include updated image urls

--- server/seed/items.ts
+++ server/seed/items.ts
@@ -4,9 +4,9 @@ export const items = [
     content:
       "Life was like a box of chocolates. You never know what you're gonna get.",
     image:
-      "https://res.cloudinary.com/jq/image/upload/v1596371860/apollo-quotes/nptaosswcqnv2nkoeezd.jpg",
+      "https://res.cloudinary.com/jq/image/upload/v1596388449/apollo-quotes/bleff1tca1mj3k1x46ao.jpg",
     largeImage:
-      "https://res.cloudinary.com/jq/image/upload/c_scale,h_600,q_auto/v1596371860/apollo-quotes/nptaosswcqnv2nkoeezd.jpg",
+      "https://res.cloudinary.com/jq/image/upload/c_scale,h_600,q_auto:best/v1596388449/apollo-quotes/bleff1tca1mj3k1x46ao.jpg",
     tags: ["life", "movie"],
   },
   {
@@ -14,18 +14,18 @@ export const items = [
     content:
       "The worst part of having a mental illness is people expect you to behave as if you don't.",
     image:
-      "https://res.cloudinary.com/jq/image/upload/v1596371856/apollo-quotes/ixq2zyvlnskcbdsuol1c.jpg",
+      "https://res.cloudinary.com/jq/image/upload/v1596388443/apollo-quotes/czcubghs9tlkfkpp3ibe.jpg",
     largeImage:
-      "https://res.cloudinary.com/jq/image/upload/c_scale,h_600,q_auto/v1596371856/apollo-quotes/ixq2zyvlnskcbdsuol1c.jpg",
+      "https://res.cloudinary.com/jq/image/upload/c_scale,h_600,q_auto:best/v1596388443/apollo-quotes/czcubghs9tlkfkpp3ibe.jpg",
     tags: ["mental illness", "movie"],
   },
   {
     author: "Wayne Gretzky",
     content: "You miss 100% of the shots you don't take.",
     image:
-      "https://res.cloudinary.com/jq/image/upload/v1596371866/apollo-quotes/zhv6mle6dh8ridhnmmnw.jpg",
+      "https://res.cloudinary.com/jq/image/upload/v1596388455/apollo-quotes/et2m6vqx1u2e3xgkcuwy.jpg",
     largeImage:
-      "https://res.cloudinary.com/jq/image/upload/c_scale,h_600,q_auto/v1596371866/apollo-quotes/zhv6mle6dh8ridhnmmnw.jpg",
+      "https://res.cloudinary.com/jq/image/upload/c_scale,h_600,q_auto:best/v1596388455/apollo-quotes/et2m6vqx1u2e3xgkcuwy.jpg",
     tags: ["inspiration", "sport"],
   },
   {
@@ -33,9 +33,9 @@ export const items = [
     content:
       "You are in danger of living a life so comfortable and soft, that you will die without ever realizing your true potential.",
     image:
-      "https://res.cloudinary.com/jq/image/upload/v1596371857/apollo-quotes/rih38x07k2kpkz95e2k8.jpg",
+      "https://res.cloudinary.com/jq/image/upload/v1596388443/apollo-quotes/ugsnnt6qo6lj8enjh4eb.jpg",
     largeImage:
-      "https://res.cloudinary.com/jq/image/upload/c_scale,h_600,q_auto/v1596371857/apollo-quotes/rih38x07k2kpkz95e2k8.jpg",
+      "https://res.cloudinary.com/jq/image/upload/c_scale,h_600,q_auto:best/v1596388443/apollo-quotes/ugsnnt6qo6lj8enjh4eb.jpg",
     tags: ["inspiration", "mental toughness"],
   },
   {
@@ -43,9 +43,9 @@ export const items = [
     content:
       "Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma -- which is living with the results of other people's thinking.",
     image:
-      "https://res.cloudinary.com/jq/image/upload/v1596371901/apollo-quotes/ko2sxnmdvsmbcxexa8or.jpg",
+      "https://res.cloudinary.com/jq/image/upload/v1596388449/apollo-quotes/ky6acj6ktfrxt6iebc7k.jpg",
     largeImage:
-      "https://res.cloudinary.com/jq/image/upload/c_scale,h_600,q_auto/v1596371901/apollo-quotes/ko2sxnmdvsmbcxexa8or.jpg",
+      "https://res.cloudinary.com/jq/image/upload/c_scale,h_600,q_auto:best/v1596388449/apollo-quotes/ky6acj6ktfrxt6iebc7k.jpg",
     tags: ["life", "motivation"],
   },
   {
@@ -53,9 +53,9 @@ export const items = [
     content:
       "The art of war teaches us to rely not on the likelihood of the enemy's not coming, but on our own readiness to receive him; not on the chance of his not attacking, but rather on the fact that we have made our position unassailable.",
     image:
-      "https://res.cloudinary.com/jq/image/upload/v1596371883/apollo-quotes/nztplykkunrlm0iumkus.jpg",
+      "https://res.cloudinary.com/jq/image/upload/v1596388471/apollo-quotes/hizx5pb1pzc3avd88bql.jpg",
     largeImage:
-      "https://res.cloudinary.com/jq/image/upload/c_scale,h_600,q_auto/v1596371883/apollo-quotes/nztplykkunrlm0iumkus.jpg",
+      "https://res.cloudinary.com/jq/image/upload/c_scale,h_600,q_auto:best/v1596388471/apollo-quotes/hizx5pb1pzc3avd88bql.jpg",
     tags: ["war", "preparedness"],
   },
   {
@@ -63,9 +63,9 @@ export const items = [
     content:
       "Twenty years from now you will be more disappointed by the things that you didn’t do than by the ones you did do.",
     image:
-      "https://res.cloudinary.com/jq/image/upload/v1596371896/apollo-quotes/nqmffvvhhbcz8rrmhkvb.jpg",
+      "https://res.cloudinary.com/jq/image/upload/v1596388448/apollo-quotes/hc1crxfbzofyvlxicuq9.jpg",
     largeImage:
-      "https://res.cloudinary.com/jq/image/upload/c_scale,h_600,q_auto/v1596371896/apollo-quotes/nqmffvvhhbcz8rrmhkvb.jpg",
+      "https://res.cloudinary.com/jq/image/upload/c_scale,h_600,q_auto:best/v1596388448/apollo-quotes/hc1crxfbzofyvlxicuq9.jpg",
     tags: ["regret", "inspiration"],
   },
   {
@@ -73,9 +73,9 @@ export const items = [
     content:
       "You must be shapeless, formless, like water. When you pour water in a cup, it becomes the cup. When you pour water in a bottle, it becomes the bottle. When you pour water in a teapot, it becomes the teapot. Water can drip and it can crash. Become like water my friend.",
     image:
-      "https://res.cloudinary.com/jq/image/upload/v1596371856/apollo-quotes/gdu3u9d42gbd1sbytfdc.jpg",
+      "https://res.cloudinary.com/jq/image/upload/v1596388441/apollo-quotes/lhs8gsablw2a6qtszr7a.jpg",
     largeImage:
-      "https://res.cloudinary.com/jq/image/upload/c_scale,h_600,q_auto/v1596371856/apollo-quotes/gdu3u9d42gbd1sbytfdc.jpg",
+      "https://res.cloudinary.com/jq/image/upload/c_scale,h_600,q_auto:best/v1596388441/apollo-quotes/lhs8gsablw2a6qtszr7a.jpg",
     tags: ["present", "adaptable", "legend"],
   },
 ];
