95dfe0f8e9404e2b47c16187d9fcfc12d2ab538a
Jeffrey Quan <jeffrey.quan@hotmail.com>
2020-08-15 18:27:30 +1000

    💄 updated max-width of form container and changed height to min-height

--- client/styles/FormContainer.ts
+++ client/styles/FormContainer.ts
@@ -1,10 +1,10 @@
 import styled from "styled-components";
 
 export const FormContainer = styled.div`
-  max-width: 350px;
+  max-width: 50rem;
   margin: 0 auto;
   display: flex;
   justify-content: center;
   align-items: center;
-  height: 100vh;
+  min-height: 100vh;
 `;
