442241bd1e7c0b17b6add22385c9b5329df82e51
Jeffrey Quan <jeffrey.quan@hotmail.com>
2020-08-15 18:22:15 +1000

    ✨ created Navbar and Header components - still work-in-progress

---
+++ client/components/Header.tsx
@@ -0,0 +1,55 @@
+import Link from "next/link";
+import { useState, useEffect } from "react";
+import styled from "styled-components";
+import { IoIosRocket } from "react-icons/io";
+import { Navbar } from "./Navbar";
+
+const Logo = styled.div`
+  font-size: 3rem;
+  margin-right: auto;
+  a {
+    display: flex;
+    align-items: center;
+  }
+`;
+
+const StyledHeader = styled.header`
+  position: sticky;
+  background-color: ${(props) =>
+    props.sticky ? props.theme.white : "transparent"};
+  box-shadow: ${(props) =>
+    props.sticky ? `0 0.2rem 0.5rem rgba(0,0,0,0.5)` : ""};
+  top: 0;
+  display: flex;
+  align-items: center;
+  transition: all 0.3s ease-in-out;
+  line-height: ${(props) => (props.sticky ? "3rem" : "5rem")};
+`;
+
+export const Header = () => {
+  const [sticky, setSticky] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 0) {
+        setSticky(true);
+      } else {
+        setSticky(false);
+      }
+    });
+  }, []);
+
+  return (
+    <StyledHeader sticky={sticky}>
+      <Logo>
+        <Link href="/">
+          <a>
+            <IoIosRocket />
+            Apollo Quotes
+          </a>
+        </Link>
+      </Logo>
+      <Navbar />
+    </StyledHeader>
+  );
+};
---
+++ client/components/Navbar.tsx
@@ -0,0 +1,41 @@
+import Link from "next/link";
+import styled from "styled-components";
+import { User } from "./User";
+
+const NavbarStyles = styled.nav`
+  font-size: 2rem;
+  ul {
+    display: flex;
+
+    a {
+      color: ${(props) => props.theme.headline2};
+      padding: 2rem 1rem;
+    }
+  }
+`;
+
+export const Navbar = () => {
+  const user = User();
+  console.log(user);
+  return (
+    <NavbarStyles>
+      <ul>
+        <Link href="/quotes">
+          <a>Quotes</a>
+        </Link>
+        {user && (
+          <>
+            <Link href="/quotes/new">
+              <a>New</a>
+            </Link>
+          </>
+        )}
+        {!user && (
+          <Link href="/login">
+            <a>Login</a>
+          </Link>
+        )}
+      </ul>
+    </NavbarStyles>
+  );
+};
