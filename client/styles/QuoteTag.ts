1999b3f29f956fbe2ed9199291db139349ea80e6
Jeffrey Quan <jeffrey.quan@hotmail.com>
2020-08-18 00:19:52 +1000

    💄 hover style to tag

--- client/styles/QuoteTag.ts
+++ client/styles/QuoteTag.ts
@@ -5,8 +5,16 @@ export const QuoteTag = styled.div`
   font-size: 1.2rem;
   background-color: ${(props) => props.theme.cardtagbg};
   color: ${(props) => props.theme.cardtagtext};
+  border: 1px solid ${(props) => props.theme.cardtagbg};
   padding: 0.5rem 1rem;
   border-radius: 3px;
   margin-right: 0.6rem;
   margin-bottom: 0.6rem;
+  cursor: pointer;
+
+  &:hover {
+    background: ${(props) => props.theme.bg2};
+    color: ${(props) => props.theme.headline2};
+    border: 1px solid ${(props) => props.theme.cardtagbg};
+  }
 `;
