8b4f497f63823348d61bcf2acf37c237b99d8a9c
Jeffrey Quan <jeffrey.quan@hotmail.com>
2020-08-22 16:32:07 +1000

    🎨 refactored validateInputs function to be re-used for sign in

--- client/lib/useForm.ts
+++ client/lib/useForm.ts
@@ -45,3 +45,49 @@ export const useForm = (initialState: FormState = {}) => {
     clearForm,
   };
 };
+
+export const validateInputs = (inputs) => {
+  let errors: any = {};
+
+  const { name, username, email, password, confirmationPassword } = inputs;
+
+  const nameRegex = /^[A-Za-z]+((\s)?((\'|\-|\.)?([A-Za-z])+))*$/;
+
+  if (!!name && name.length === 0) {
+    errors.name = "Name cannot be empty";
+  } else if (!nameRegex.test(name)) {
+    errors.name = "Name is invalid";
+  }
+
+  const userNameRegex = /^[a-zA-Z][a-zA-Z0-9_]{3,15}$/;
+
+  if (!!username && username.length === 0) {
+    errors.username = "Username cannot be empty";
+  } else if (!userNameRegex.test(username)) {
+    errors.username =
+      "Username must begin with a letter and be between 4 to 16 characters long";
+  }
+
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  if (!!email && email.length === 0) {
+    errors.email = "Email cannot be empty";
+  } else if (!emailRegex.test(email)) {
+    errors.email = "Email is invalid";
+  }
+
+  if (!!password && password.length === 0) {
+    errors.password = "Password cannot be empty";
+  } else if (password.length < 6) {
+    errors.password = "Password must be at least 6 characters";
+  }
+
+  if (!!confirmationPassword && confirmationPassword.length === 0) {
+    errors.confirmationPassword = "Confirmation Password cannot be empty";
+  } else if (!!confirmationPassword && password !== confirmationPassword) {
+    errors.confirmationPassword =
+      "Password and Confirmation Password must match";
+  }
+
+  return errors;
+};
