57062799fb46e75d35a96538cc9d45cb41e77b5b
Jeffrey Quan <jeffrey.quan@hotmail.com>
2020-09-05 15:10:57 +1000

    💄 added styling to message if error prop is present

--- client/components/Message.tsx
+++ client/components/Message.tsx
@@ -1,9 +1,23 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 const MessageStyles = styled.div`
+  position: absolute;
+  top: 9rem;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
   height: 2.4rem;
+  padding: 2rem;
+
+  ${(props) =>
+    props.error &&
+    css`
+      border: 1px solid red;
+      border-radius: 4px;
+    `}
 `;
 
-export const Message = ({ children }) => {
-  return <MessageStyles>{children}</MessageStyles>;
+export const Message = ({ children, ...props }) => {
+  return <MessageStyles {...props}>{children}</MessageStyles>;
 };
