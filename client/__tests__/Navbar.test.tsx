ec8a93a6e6e665598378fe2b3fde887a5ecd735e
Jeffrey Quan <jeffrey.quan@hotmail.com>
2020-09-14 20:04:32 +1000

    🐛 fixed tests to use text instead of role

--- client/__tests__/Navbar.test.tsx
+++ client/__tests__/Navbar.test.tsx
@@ -1,10 +1,20 @@
 import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { MockedProvider } from "@apollo/client/testing";
+import { useRouter } from "next/router";
+import Link from "next/link";
+import { RouterContext } from "next/dist/next-server/lib/router-context";
 import { Navbar } from "../components/Navbar";
 import { AuthProvider } from "../components/Auth";
 import { CURRENT_USER_QUERY } from "../components/Auth";
 
 let mutationWasCalled = false;
+
+jest.mock("next/link", () => {
+  return ({ children }) => {
+    return children;
+  };
+});
 
 const userSignedInMocks = [
   {
@@ -40,22 +50,22 @@ const userNotSignedInMocks = [
 
 describe("Navbar", () => {
   it("shows home and sign in icons when not signed in", async () => {
-    const { getByRole, queryByRole } = render(
+    const { getByText, queryByText } = render(
       <MockedProvider mocks={userNotSignedInMocks}>
         <Navbar />
       </MockedProvider>
     );
 
-    expect(getByRole("link", { name: "Quotes" })).toBeInTheDocument();
-    expect(getByRole("link", { name: "Sign In" })).toBeInTheDocument();
-    expect(queryByRole("link", { name: "My Quotes" })).not.toBeInTheDocument();
-    expect(queryByRole("link", { name: "Likes" })).not.toBeInTheDocument();
-    expect(queryByRole("link", { name: "New" })).not.toBeInTheDocument();
-    expect(queryByRole("link", { name: "Log out" })).not.toBeInTheDocument();
+    expect(getByText("Quotes")).toBeInTheDocument();
+    expect(getByText("Sign In")).toBeInTheDocument();
+    expect(queryByText("My Quotes")).not.toBeInTheDocument();
+    expect(queryByText("Likes")).not.toBeInTheDocument();
+    expect(queryByText("New")).not.toBeInTheDocument();
+    expect(queryByText("Log out")).not.toBeInTheDocument();
   });
 
   it("shows home, my quotes, likes, new and logout icons whened not signed in", async () => {
-    const { getByRole, queryByRole } = render(
+    const { getByText, queryByText } = render(
       <MockedProvider mocks={userSignedInMocks}>
         <AuthProvider>
           <Navbar />
@@ -67,13 +77,61 @@ describe("Navbar", () => {
       expect(mutationWasCalled).toBe(true);
     });
 
-    expect(getByRole("link", { name: "Quotes" })).toBeInTheDocument();
-    await waitFor(() =>
-      expect(queryByRole("link", { name: "Sign In" })).not.toBeInTheDocument()
-    );
-    expect(getByRole("link", { name: "My Quotes" })).toBeInTheDocument();
-    expect(getByRole("link", { name: "Likes" })).toBeInTheDocument();
-    expect(getByRole("link", { name: "New" })).toBeInTheDocument();
+    expect(getByText("Quotes")).toBeInTheDocument();
+
+    await waitFor(() => expect(queryByText("Sign In")).not.toBeInTheDocument());
+
+    expect(getByText("My Quotes")).toBeInTheDocument();
+    expect(getByText("Likes")).toBeInTheDocument();
+    expect(getByText("New")).toBeInTheDocument();
     expect(screen.getByText("Log out")).toBeInTheDocument();
   });
+  // TODO: work out how to test route changes
+  // it("navigates to the correct routes for each icon when clicked", async () => {
+  //   const router = {
+  //     basePath: "",
+  //     pathname: "/",
+  //     route: "/",
+  //     asPath: "/",
+  //     query: {},
+  //     push: jest.fn(),
+  //     replace: jest.fn(),
+  //     reload: jest.fn(),
+  //     back: jest.fn(),
+  //     prefetch: jest.fn(),
+  //     beforePopState: jest.fn(),
+  //     events: {
+  //       on: jest.fn(),
+  //       off: jest.fn(),
+  //       emit: jest.fn(),
+  //     },
+  //     isFallback: false,
+  //   };
+
+  //   const { getByText, queryByText } = render(
+  //     <MockedProvider mocks={userSignedInMocks}>
+  //       <AuthProvider>
+  //         <RouterContext.Provider value={router}>
+  //           <Navbar />
+  //         </RouterContext.Provider>
+  //       </AuthProvider>
+  //     </MockedProvider>
+  //   );
+
+  //   await waitFor(() => {
+  //     expect(mutationWasCalled).toBe(true);
+  //   });
+
+  //   await userEvent.click(getByText("Quotes"));
+
+  //   await waitFor(() => {
+  //     expect(router.pathname).toBe("/");
+  //   });
+
+  //   await userEvent.click(getByText("My Quotes"));
+
+  //   await waitFor(() => {
+  //     expect(router.pathname).toBe(`/user/jeffrey/quotes`);
+  //   });
+  // });
 });
