c3ce18297cc5b15fbce9ff4f81b658a3e47b46af
Jeffrey Quan <jeffrey.quan@hotmail.com>
2020-09-16 16:54:02 +1000

    🎨 convert fake quotes and cache into functions that returned these items

--- client/lib/testUtils.ts
+++ client/lib/testUtils.ts
@@ -6,7 +6,7 @@ function decodeCursor(encodedCursor: string) {
   return Buffer.from(encodedCursor, "base64").toString("ascii");
 }
 
-const testQuote = {
+const fakeQuoteWithoutUserLike = () => ({
   id: "testquote123",
   author: "Forrest Gump, Forrest Gump",
   content:
@@ -30,86 +30,123 @@ const testQuote = {
     { id: "abf", name: "life", __typename: "Tag" },
   ],
   submittedBy: {
+    id: "abc123",
+    __typename: "User",
+  },
+  slug: "forrest-gump",
+  __typename: "Quote",
+});
+
+const fakeQuoteWithUserLike = () => ({
+  id: "testquote124",
+  author: "Forrest Gump, Forrest Gump",
+  content:
+    "Life was like a box of chocolates. You never know what you're gonna get.",
+  image: "https://www.fillmurray.com/200/300",
+  largeImage: "https://www.fillmurray.com/200/300",
+  likes: [
+    {
+      id: "abd",
+      user: { id: "abc123", username: "jeffrey", __typename: "User" },
+      __typename: "Like",
+    },
+    {
+      id: "abc",
+      user: { id: "abd123", username: "John", __typename: "User" },
+      __typename: "Like",
+    },
+    {
+      id: "abd",
+      user: { id: "abe123", username: "Ben", __typename: "User" },
+      __typename: "Like",
+    },
+  ],
+  tags: [
+    { id: "abe", name: "movie", __typename: "Tag" },
+    { id: "abf", name: "life", __typename: "Tag" },
+  ],
+  submittedBy: {
     id: "abce1234",
     __typename: "User",
   },
   slug: "forrest-gump",
   __typename: "Quote",
-};
-
-const testCache = new InMemoryCache({
-  typePolicies: {
-    Query: {
-      fields: {
-        quotes: {
-          keyArgs: ["tag", "submittedBy", "likedBy"],
-          merge: (
-            existing = {
-              __typename: "QuotesConnection",
-              totalCount: 0,
-              pageInfo: {
-                endCursor: null,
-                hasMore: true,
-              },
-              quotes: [],
-            },
-            incoming
-          ) => {
-            let existingCursor;
-            let incomingCursor;
-
-            if (
-              existing.pageInfo.endCursor &&
-              incoming.pageInfo.endCursor !== NEW_QUOTE
-            ) {
-              existingCursor = decodeCursor(existing.pageInfo.endCursor);
-
-              incomingCursor = decodeCursor(incoming.pageInfo.endCursor);
-            }
-
-            if (incomingCursor > existingCursor) {
-              return existing;
-            }
-
-            const newQuotes = incoming.quotes;
-
-            let update;
-
-            if (incoming.pageInfo.endCursor === NEW_QUOTE) {
-              update = {
-                pageInfo: existing.pageInfo,
-                quotes: [...newQuotes, ...existing.quotes],
-                totalCount: incoming.totalCount,
-              };
-            } else {
-              update = {
-                quotes: [...existing.quotes, ...newQuotes],
-              };
-            }
-
-            return newQuotes.length > 0
-              ? {
-                  ...incoming,
-                  ...update,
-                }
-              : existing;
-          },
-        },
-      },
-    },
-    Quote: {
-      fields: {
-        likes: {
-          merge(existing = [], incoming) {
-            return incoming;
-          },
-        },
-      },
-    },
-  },
 });
 
-const testNewQuote = {
+const fakeCache = () =>
+  new InMemoryCache({
+    typePolicies: {
+      Query: {
+        fields: {
+          quotes: {
+            keyArgs: ["tag", "submittedBy", "likedBy"],
+            merge: (
+              existing = {
+                __typename: "QuotesConnection",
+                totalCount: 0,
+                pageInfo: {
+                  endCursor: null,
+                  hasMore: true,
+                },
+                quotes: [],
+              },
+              incoming
+            ) => {
+              let existingCursor;
+              let incomingCursor;
+
+              if (
+                existing.pageInfo.endCursor &&
+                incoming.pageInfo.endCursor !== NEW_QUOTE
+              ) {
+                existingCursor = decodeCursor(existing.pageInfo.endCursor);
+
+                incomingCursor = decodeCursor(incoming.pageInfo.endCursor);
+              }
+
+              if (incomingCursor > existingCursor) {
+                return existing;
+              }
+
+              const newQuotes = incoming.quotes;
+
+              let update;
+
+              if (incoming.pageInfo.endCursor === NEW_QUOTE) {
+                update = {
+                  pageInfo: existing.pageInfo,
+                  quotes: [...newQuotes, ...existing.quotes],
+                  totalCount: incoming.totalCount,
+                };
+              } else {
+                update = {
+                  quotes: [...existing.quotes, ...newQuotes],
+                };
+              }
+
+              return newQuotes.length > 0
+                ? {
+                    ...incoming,
+                    ...update,
+                  }
+                : existing;
+            },
+          },
+        },
+      },
+      Quote: {
+        fields: {
+          likes: {
+            merge(existing = [], incoming) {
+              return incoming;
+            },
+          },
+        },
+      },
+    },
+  });
+
+const fakeNewQuote = () => ({
   id: "abcd123",
   author: "CS",
   content: "Hello World",
@@ -121,6 +158,11 @@ const testNewQuote = {
   ],
   likes: [],
   slug: "hello-world-cs",
-};
+});
 
-export { testCache, testQuote, testNewQuote };
+export {
+  fakeCache,
+  fakeQuoteWithoutUserLike,
+  fakeQuoteWithUserLike,
+  fakeNewQuote,
+};
